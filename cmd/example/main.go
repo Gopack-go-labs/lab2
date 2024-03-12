70e5d5002e09c009776921f661de564c731b43b9
erotourtes <erotourtes@gmail.com>
2024-03-12 12:51:09 +0200

    Changed module name

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"github.com/Gopack-go-labs/lab2"
 )
 
 var (
@@ -22,6 +22,6 @@ func main() {
 	//       }
 	//       err := handler.Compute()
 
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
+	res, _ := lab2.PostfixToInfix("+ 2 2")
 	fmt.Println(res)
 }
