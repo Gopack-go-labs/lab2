83cb571042c2ddfb431e0a4f80ca7c699b3f5a05
erotourtes <erotourtes@gmail.com>
2024-03-12 12:54:08 +0200

    Added implementation

--- implementation.go
+++ implementation.go
@@ -1,9 +1,64 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+var operators = map[string]int{
+	"^": 1,
+	"*": 2,
+	"/": 2,
+	"+": 3,
+	"-": 3,
+}
+
+func PostfixToInfix(input string) (string, error) {
+	tokens := strings.Split(input, " ")
+	if len(tokens) == 0 {
+		return "", nil
+	}
+
+	var stack []string
+	prevPrecedence := 0
+	for _, token := range tokens {
+		if precedence, isOperator := operators[token]; isOperator {
+			if len(stack) < 2 {
+				return "", fmt.Errorf("invalid postfix expression")
+			}
+
+			operator := token
+			operand1 := stack[len(stack)-2]
+			operand2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+
+			if isNegative := strings.HasPrefix(operand2, "-"); isNegative {
+				if (operator) == "+" {
+					operator = "-"
+					operand2 = operand2[1:]
+				} else if (operator) == "-" {
+					operator = "+"
+					operand2 = operand2[1:]
+				}
+			}
+
+			if prevPrecedence > precedence {
+				// if operand1 is only a number, no need to wrap it in parentheses
+				if _, err := strconv.Atoi(operand1); err != nil {
+					operand1 = fmt.Sprintf("(%s)", operand1)
+				}
+				if _, err := strconv.Atoi(operand2); err != nil {
+					operand2 = fmt.Sprintf("(%s)", operand2)
+				}
+			}
+			prevPrecedence = precedence
+
+			stack = append(stack, fmt.Sprintf("%s %s %s", operand1, operator, operand2))
+		} else {
+			stack = append(stack, token)
+		}
+	}
+
+	return stack[0], nil
 }
--- implementation_test.go
+++ implementation_test.go
@@ -7,17 +7,51 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+func TestPostfixToInfix(t *testing.T) {
+	for _, tt := range []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "empty input",
+			input:    "",
+			expected: "",
+		},
+		{
+			name:     "negative 1",
+			input:    "4 -2 + 3 +",
+			expected: "4 - 2 + 3",
+		},
+		{
+			name:     "negative 2",
+			input:    "4 -2 - 3 +",
+			expected: "4 + 2 + 3",
+		},
+		{
+			name:     "postfix 1",
+			input:    "4 2 - 3 * 5 2 3 + ^ +",
+			expected: "(4 - 2) * 3 + 5 ^ (2 + 3)",
+		},
+		{
+			name:     "postfix 2",
+			input:    "5 2 3 4 ^ + ^",
+			expected: "5 ^ (2 + 3 ^ 4)",
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := PostfixToInfix(tt.input)
+			if assert.Nil(t, err) {
+				assert.Equal(t, tt.expected, res)
+			}
+		})
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
+func ExamplePostfixToInfix() {
+	res, _ := PostfixToInfix("+ 2 2")
 	fmt.Println(res)
 
 	// Output:
-	// 2 2 +
+	// 2 + 2
 }
